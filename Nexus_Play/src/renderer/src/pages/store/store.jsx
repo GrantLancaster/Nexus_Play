313fe60df5ab7c60c215f72407b43235b9a287aa
Jyotish547 <jyotish547@gmail.com>
2023-12-02 18:32:44 -0700

    Router added

---
+++ Nexus_Play/src/renderer/src/pages/store/store.jsx
@@ -0,0 +1,14 @@
+
+export default function Store() {
+    //Still need colors for the header buttons
+    return (
+      <>
+        {/* Background Overlay */}
+        <div className="overlay"></div>
+  
+        <HomePage />
+      </>
+      
+    )
+  }
+  
